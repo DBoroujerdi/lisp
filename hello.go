a147fbb335a35d0304633f5614dc580e72c1e933
Daniel Boroujerdi <daniel.boroujerdi@williamhill.com>
2014-04-11 14:35:28 +0100

    golist repl

--- hello.go
+++ hello.go
@@ -1,7 +1,23 @@
 package main
 
 import "fmt"
+import "bufio"
+import "os"
 
 func main() {
-	fmt.Printf("hello, world\n")
+	fmt.Printf("GoLisp Version 0.1\n")
+	fmt.Printf("Press Ctrl+c to Exit\n")
+
+	reader := bufio.NewReader(os.Stdin)
+
+	for {
+		input, err := reader.ReadString('\n')
+
+		if err != nil {
+			// You may check here if err == io.EOF
+			break
+		}
+
+		fmt.Printf(input)
+	}
 }
