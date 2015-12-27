3f013a7c1e47d75e35854056408634c4607d4b88
Daniel Boroujerdi <daniel.boroujerdi@gmail.com>
2015-12-27 22:48:30 +0000

    super duper basic lexer

---
+++ lexer/lexer.go
@@ -0,0 +1,100 @@
+package lexer
+
+import "unicode/utf8"
+import "fmt"
+
+type TokenType int
+
+type Token struct {
+	typ TokenType
+	val interface{}
+}
+
+const (
+	// specials
+	NewLine = iota
+	EOF
+
+	// parens
+	OpenBracket
+	CloseBracket
+
+	// types
+	Integer
+
+	// operators
+	Plus
+	Minus
+)
+
+type Lexer struct {
+	input string
+}
+
+// public
+
+// NewLexer Lexer constructor
+func NewLexer(input string) *Lexer {
+	l := new(Lexer)
+	l.input = input
+
+	return l
+}
+
+// GetTokens get token
+func (lexer *Lexer) GetTokens() ([]*Token, error) {
+	_ = "breakpoint"
+	var tokens []*Token
+
+	for lexer.inputBytesRemaining() > 0 {
+		token, err := lexer.nextToken()
+
+		if err != nil {
+			return nil, err
+		}
+
+		tokens = append(tokens, token)
+	}
+
+	return tokens, nil
+}
+
+// private
+
+func (lexer *Lexer) inputBytesRemaining() int64 {
+	return int64(len(lexer.input))
+}
+
+func (lexer *Lexer) nextToken() (*Token, error) {
+	_ = "breakpoint"
+
+	r, size := utf8.DecodeRuneInString(lexer.input)
+	lexer.input = lexer.input[size:]
+
+	token := new(Token)
+
+	switch r {
+	case ' ':
+		nextToken, err := lexer.nextToken()
+
+		if err != nil {
+			token = nextToken
+			break
+		}
+		return nil, err
+
+	case '1', '2', '3', '4', '5', '6', '7', '8', '9':
+		token.typ = Integer
+		token.val = int(r)
+	case '(':
+		token.typ = OpenBracket
+	case ')':
+		token.typ = CloseBracket
+	case '+':
+		token.typ = Plus
+	default:
+		return nil, fmt.Errorf("Unknown token %#U", r)
+	}
+
+	return token, nil
+}
---
+++ lexer/lexer_test.go
@@ -0,0 +1,35 @@
+package lexer
+
+import "testing"
+
+func TestLexerConstructor(t *testing.T) {
+	lexer := NewLexer("(+ 2 2)")
+
+	if lexer.input != "(+ 2 2)" {
+		t.Error("lexer.input was not initialized properly", lexer)
+	}
+}
+
+func TestGetTokens(t *testing.T) {
+	lexer := NewLexer("(+ 2 2)")
+
+	tokens, err := lexer.GetTokens()
+
+	if err != nil {
+		t.Error(err)
+	}
+
+	if len(tokens) != 5 {
+		t.Error("Incorrect number of tokens", tokens)
+	}
+}
+
+func TestGetTokensWithEnexpectedRune(t *testing.T) {
+	lexer := NewLexer("(& 2 2)")
+
+	_, err := lexer.GetTokens()
+
+	if err == nil {
+		t.Error("Error expected")
+	}
+}
